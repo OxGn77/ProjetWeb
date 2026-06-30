8fa342b91277a5f229b087fb4f60908c6d3f5e99
Kill VDH <killian.vandenhole@gmail.com>
2026-06-30 13:00:19 +0200

    commit3

--- Controller/ticket.controller.php
+++ Controller/ticket.controller.php
@@ -3,7 +3,19 @@ include_once('./Model/ticket.model.php');
     Class ticketController{
         public function getAll(){
             $tickets = ticket::getAll();
-           header('Content-Type: application/json');
-        echo json_encode($tickets);
+            include_once('./View/ticketlist.view.php');
+        }
+        public function getById($id){
+            $tickets = ticket::getById($id);
+            include_once('./View/ticketlist.view.php');
+        }
+        public function create(){
+            $data = file_get_contents('php://input');
+            $ticket = json_decode($data);
+            ticket::create($ticket);
+        }
+        public function updateStatus($id){
+            $data = json_decode(file_get_contents("php://input"), true);
+            ticket::updateStatus($id, $data["statut"]);
         }
     }
--- View/ticketlist.view.php
+++ View/ticketlist.view.php
@@ -1,20 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php foreach($tickets as $ticket){ ?>
-    <div class="ticket">
-        <div class="id"><?= $ticket->id ?></div>
-        <div class="titre"><?= $ticket->titre ?></div>
-        <div class="categorie"><?= $ticket->categorie ?></div>
-        <div class="priorite"><?= $ticket->priorite ?></div>
-        <div class="statut"><?= $ticket->statut ?></div>
-    </div>
-    <?php } ?>
-
-</body>
-</html>
+<?php
+header('Content-Type: application/json');
+echo json_encode($tickets);
