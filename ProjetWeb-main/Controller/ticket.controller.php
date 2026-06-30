3dc53280112d294c91b9f53be5a5ddb7593d2274
Kill VDH <killian.vandenhole@gmail.com>
2026-06-30 12:12:35 +0200

    commit

--- ProjetWeb-main/Controller/ticket.controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once('./ticket.model.php');
-    Class ticketController{
-        public function getAll(){
-            $tickets = ticket::getAll();
-            include('./view/ticketlist.view.php');
-        }
-    }
